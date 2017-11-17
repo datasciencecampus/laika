46716e20555293146db010242649dda65af7ae27
Phil Stubbings <phil@parasec.net>
2017-11-17 16:11:38 +0000

    closes #21

--- data.py
+++ data.py
@@ -3,9 +3,10 @@ import numpy as np
 import pandas as pd
 import cv2
 import h5py
+import os
 
 
-def load_image(img_location, height, width):
+def load_skynet_image(img_location, height, width):
     """load and pre-process satellite image.
 
     the result is a matrix of shape: (3, height, width), where 3 channels
@@ -13,6 +14,7 @@ def load_image(img_location, height, width):
 
     the matrix is normalised within range [0, 1].
     """
+    print("Processing image {}.".format(img_location))
     # satellite image 
     sample_img = cv2.imread(img_location)
     assert sample_img.shape == (height, width, 3)
@@ -28,7 +30,7 @@ def load_image(img_location, height, width):
     return sample_img
 
 
-def load_label(label_location, height, width, num_classes):
+def load_skynet_label(label_location, height, width, num_classes):
     """load and process the associated satellite image labels.
 
     the labels have been encoded as images, where pixel BGR values correspond to
@@ -46,6 +48,7 @@ def load_label(label_location, height, width, num_classes):
           ...
           m[:, 255] = binary vector for class 255.
     """
+    print("Processing label {}.".format(label_location))
     # labels encoded as an image.
     label_img = cv2.imread(label_location)
     assert label_img.shape == (height, width, 3)
@@ -68,46 +71,47 @@ def load_skynet_data(skynet_data="../skynet-data/data", width=256, height=256):
  
     result is a tuple of 2 matrices (images, labels) with 1 row per image.
     """
+    # skynet images with _at least_ 1 labelled pixel.
+    non_empty = pd.read_csv(skynet_data + "/sample-filtered.txt", sep=" ", header=None)
 
+    ### images
+    images = [load_skynet_image("{}/images/{}-{}-{}.jpg".format(skynet_data, *row[1:4]), height, width) for _, row in non_empty.iterrows()]
+
+    ### labels
     # skynet class definitions
     with open(skynet_data + "/classes.json") as f:
         classes = json.load(f)
 
-    # images with _at least_ 1 labelled pixel.
-    non_empty = pd.read_csv(skynet_data + "/sample-filtered.txt", sep=" ", header=None)
-
     # 1 extra class for "background" (unlabelled)
     num_classes = len(classes) + 1
-
-    images = []
-    labels = []
-    
-    for i, row in non_empty.iterrows():
-    
-        print("processing image/mask {:06d}".format(i))
-   
-        sample_img = load_image("{}/images/{}-{}-{}.jpg".format(skynet_data, *row[1:4]), height, width)
-        images.append(sample_img)
-
-        masks = load_label("{}/labels/color/{}-{}-{}.png".format(skynet_data, *row[1:4]), height, width, num_classes)
-        labels.append(masks)
+    labels = [load_skynet_label("{}/labels/color/{}-{}-{}.png".format(skynet_data, *row[1:4]), height, width, num_classes) for _, row in non_empty.iterrows()]
 
     return np.array(images), np.array(labels)
 
 
-def load_hdf5_data(img_src="data/images.hdf5", labels_src="data/labels.hdf5"):
-    """load image and label data from hdf5 file."""
-    pass
+def load_data(skynet_data="../skynet-data/data", hdf5_data="data/training.hdf5", width=256, height=256):
+    """load training data.
+   
+    will load pre-processed data in data/training.hdf5 (if exists).
+    else will do pre-processing, cache result and return.
 
-
-def load_data():
-    """load image and label data."""
-    pass
-
-
-def save_data(images, labels, dst="data/"):
-    """persist image and label data."""
-    pass
+    result is a tuple of 2 matrices (images, labels) with 1 row per image.
+    """
+    if not os.path.isfile(hdf5_data):
+        print("Loading training data from skynet.")
+        images, labels = load_skynet_data(skynet_data, width, height)
+        hdf5 = h5py.File(hdf5_data, "w")
+        hdf5.create_dataset("images", data=images)
+        hdf5.create_dataset("labels", data=labels)
+        hdf5.close()
+        return images, labels
+    else:
+        print("Loading training data from HDF5.")
+        hdf5 = h5py.File(hdf5_data, "r")
+        images = hdf5.get("images")[()]
+        labels = hdf5.get("labels")[()]
+        hdf5.close()
+        return images, labels 
 
 
 if __name__ == "__main__":
