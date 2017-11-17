903815006a4b33e375bdb7b2e7cfd31ea1e58b01
Phil Stubbings <phil@parasec.net>
2017-11-17 11:08:16 +0000

    refactor a bit. #16

--- segnet_basic.py
+++ segnet_basic.py
@@ -11,66 +11,62 @@ from keras.utils import plot_model
 from keras import backend as K
 K.set_image_data_format("channels_first")
 
-import os
-os.environ["KERAS_BACKEND"] = "theano"
-os.environ["THEANO_FLAGS"] = "mode=FAST_RUN, device=gpu0, floatX=float32, optimizer=fast_compile"
 
-img_channels = 3
-img_width = 256
-img_height = 256
+def model(img_channels=3, img_width=256, img_height=256):
+    """define a basic segnet model."""
+    model = Sequential()
 
-## model
-model = Sequential()
+    # encoder
+    model.add(ZeroPadding2D(padding=1, input_shape=(img_channels, img_height, img_width)))
+    model.add(Conv2D(filters=64, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=2))
 
-# encoder
-model.add(ZeroPadding2D(padding=1, input_shape=(img_channels, img_height, img_width)))
-model.add(Conv2D(filters=64, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=2))
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=128, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=2))
 
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=128, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=2))
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=256, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=2))
 
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=256, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=2))
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=512, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
 
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=512, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
-model.add(Activation("relu"))
+    # decoder
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=512, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
 
-# decoder
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=512, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
+    model.add(UpSampling2D(size=2))
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=256, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
 
-model.add(UpSampling2D(size=2))
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=256, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
+    model.add(UpSampling2D(size=2))
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=128, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
 
-model.add(UpSampling2D(size=2))
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=128, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
+    model.add(UpSampling2D(size=2))
+    model.add(ZeroPadding2D(padding=1))
+    model.add(Conv2D(filters=64, kernel_size=3, padding="valid"))
+    model.add(BatchNormalization())
 
-model.add(UpSampling2D(size=2))
-model.add(ZeroPadding2D(padding=1))
-model.add(Conv2D(filters=64, kernel_size=3, padding="valid"))
-model.add(BatchNormalization())
+    # output layer
+    model.add(Conv2D(filters=12, kernel_size=1, padding="valid"))
+    model.add(Reshape((12, img_width*img_height), input_shape=(12, img_height, img_width)))
+    model.add(Permute((2, 1)))
+    model.add(Activation("softmax"))
 
-# output layer
-model.add(Conv2D(filters=12, kernel_size=1, padding="valid"))
-model.add(Reshape((12, img_width*img_height), input_shape=(12, img_height, img_width)))
-model.add(Permute((2, 1)))
-model.add(Activation("softmax"))
+    return model
 
-print(model.summary())
-plot_model(model, to_file="/tmp/x.png")
+#print(model.summary())
+#plot_model(model, to_file="/tmp/x.png")
