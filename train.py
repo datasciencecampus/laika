d31e83d0a5f5a7277eef15902e57780523cd9840
Phil Stubbings <phil@parasec.net>
2017-11-21 17:08:59 +0000

    closes #25

--- train.py
+++ train.py
@@ -3,14 +3,15 @@ import os
 os.environ["KERAS_BACKEND"] = "theano"
 os.environ["THEANO_FLAGS"] = "mode=FAST_RUN, device=cuda, floatX=float32, optimizer=fast_compile"
 
+import csv
 import numpy as np
 from keras.optimizers import SGD
-from keras.callbacks import CSVLogger
+from keras.callbacks import CSVLogger, EarlyStopping
 
 from model import model
 from data import load_data
 
-epochs = 1
+epochs = 100 
 
 m = model()
 print("Loaded model.")
@@ -22,6 +23,7 @@ m.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 print("Model compiled.")
 
 images, labels = load_data()
+images, labels = images[:100], labels[:100] # dev
 
 # shuffle dataset.
 total = len(images)
@@ -36,10 +38,26 @@ images, labels = images[:-holdback], labels[:-holdback]
 print("Loaded {} instances.".format(total))
 print("Training = {}, Validation = {}".format(len(images), len(images_val)))
 
-callbacks = []
-callbacks.append(CSVLogger("train.log", separator=",", append=False))
-callbacks.append(keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0.001, patience=10, verbose=1, mode="min"))
-hist = m.fit(x=images, y=labels, validation_data=(images_val, labels_val), batch_size=16, epochs=epochs, verbose=1, callbacks=[call])
+# callbacks broken in latest keras version.
+#call = []
+#call.append(CSVLogger("train.log", separator=",", append=False))
+#call.append(EarlyStopping(monitor="val_loss", min_delta=0.001, patience=10, verbose=1, mode="min"))
+hist = m.fit(x=images, y=labels, validation_data=(images_val, labels_val), batch_size=16, epochs=epochs, verbose=1)
 print("Training complete.")
 
-print(hist)
+print("Saving weights.")
+m.save_weights("weights.hdf5")
+
+print(hist.history)
+
+print("Saving training history.")
+# {"a": [1,2,3], "b": [4,5,6]} ->
+# [[1,4], [2,5], [3,6]]
+hist_dict = hist.history
+headers = ["loss", "val_loss", "val_acc", "acc"]
+train_hist = [list(x) for x in zip(*[hist_dict[k] for k in headers])]
+
+with open("training_log.csv", "w") as f:
+    csv_writer = csv.writer(f, delimiter=",")
+    csv_writer.writerow(headers)
+    csv_writer.writerows(train_hist)
